445b0d669170f5f2224da202af0a649156e8415f
Kyle <kpete2017@gmail.com>
2020-10-23 23:34:56 -0600

    added icons to bottom tab nav bar:

--- src/components/MyDrawer.js
+++ src/components/MyDrawer.js
@@ -4,14 +4,28 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen'
 import SettingsScreen from '../screens/SettingsScreen'
 import ProfileScreen from '../screens/ProfileScreen'
-
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
   
 export default function MyDrawer() {
     return (
       <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
+            if (route.name === 'Home') {
+              iconName = 'md-home'
+            } else if(route.name === 'Profile') {
+              iconName = 'md-person'
+            } else {
+              iconName = 'md-settings'
+            }
+
+            return <Ionicons name={iconName} size={size} color="white" />;
+          },
+        })}
         style={styles.tab}
         tabBarOptions = {{
           style: {
